82bc4c3ec96a82fe231cf7e5a82da48c10c7aca4
DesertGaming <ojpereira4@gmail.com>
2026-07-27 03:26:16 -0700

    Populated preseason estimates table

---
+++ sql/seed_preseason_estmiates.sql
@@ -0,0 +1,121 @@
+-- Seeding static pre-season win estimates --
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 103.5, 103.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'LAD';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 90.5, 90.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'NYY';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 90.5, 90.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'NYM';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 89.5, 89.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'SEA';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 89.5, 89.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'PHI';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 88.5, 88.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'CHC';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 88.5, 88.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'TOR';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 87.5, 87.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'BOS';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 86.5, 86.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'HOU';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 86.5, 86.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'ATL';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 85.5, 85.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'BAL';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 85.5, 85.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'DET';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 84.5, 84.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'MIL';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 83.5, 83.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'TEX';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 83.5, 83.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'SD';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 82.5, 82.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'KC';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 80.5, 80.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'SF';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 80.5, 80.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'CIN';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 79.5, 79.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'ARI';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 79.5, 79.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'CLE';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 78.5, 78.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'PIT';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 76.5, 76.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'TB';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 75.5, 75.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'ATH';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 72.5, 72.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'MIN';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 72.5, 72.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'MIA';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 70.5, 70.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'LAA';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 69.5, 69.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'STL';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 67.5, 67.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'CWS';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 65.5, 65.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'WSH';
+
+INSERT INTO preseason_estimates (season, team_id, vegas_win_line, preseason_strength, source)
+SELECT 2026, team_id, 55.5, 55.5/162, 'BetMGM, 2026-03-24'
+FROM teams where team_abbr = 'COL';
