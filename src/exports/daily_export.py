4ea533a7049613ec2431eb333364def4a2d39e2e
DesertGaming <ojpereira4@gmail.com>
2026-08-20 16:33:31 -0700

    exports update

--- src/exports/daily_export.py
+++ src/exports/daily_export.py
@@ -6,6 +6,7 @@ from src.exports.export_matchups import export_matchups
 from src.exports.export_pmf_summary import export_summaries
 from src.exports.export_strengths import export_strengths
 from src.exports.export_wins import export_wins
+from src.exports.export_pmfs import export_pmfs
 
 today = date.today()
 season = today.year
@@ -16,6 +17,7 @@ with get_connection() as conn:
         export_strengths(cur, season)
         export_wins(cur, season)
         export_summaries(cur, season)
+        export_pmfs(cur, season)
     conn.commit()
 
 conn.close()
